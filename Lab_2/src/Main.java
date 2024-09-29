9e27551e805dc81479657700a0463b08083bfa56
Daniil Krovyakov <mr.krovyakov.daniil@gmail.com>
2024-09-29 18:04:41 +0400

    add unit tests

--- Lab_2/src/Main.java
+++ Lab_2/src/Main.java
@@ -1,5 +1,7 @@
 import functions.*;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class Main {
@@ -23,22 +25,53 @@ public class Main {
     }
 }
 
-//public class TabulatedFunctionTest{
-//    @Test
-//    public void tabulatedFunctionTest(){
-////        TabulatedFunction equation = new TabulatedFunction(-3, 3, new double[] {-2, -1, 0, 1, 2, 3, 4});
-////        equation.addPoint(new FunctionPoint(2, -5));
-////        Double[][] arr = {{-3.,-2.}, {-2., -1.}, {-1., 0.}, {0., 1.}, {1., 2.}, {2., -5.}, {3.,4.}};
-////
-////        for(int i = 0; i < equation.getPointsCount(); i++) {
-////            Assertions.assertArrayEquals(arr[i], equation[i]);
-////        }
-//        Double[][] Arr= {{-3., 2.}, {-2., -1.}, {-1., 0.}, {0., 1.}, {1., 2.}, {2., 3.}, {3., 4.}};
-//        TabulatedFunction eq =new TabulatedFunction(-3,3, new double[]{-2.,-1.,-0.,1.,2.,3.,4.});
-//        eq.addPoint(new FunctionPoint(2,-5));
-//        Double[][] arr= {{-3., 2.}, {-2., -1.}, {-1., 0.}, {0., 1.}, {1., 2.}, {2., -5.}, {3., 4.}};
-//        Assertions.assertArrayEquals(arr,Arr);
-//    }
-//
-//}
-//}
+class TabulatedFunctionTest{
+
+    TabulatedFunction eq =new TabulatedFunction(-3,3, new double[]{-2.,-1., 0.,1.,2.,3.,4.});
+
+    @Test
+    public void tabulatedFunctionAddPointTest(){
+        Double[][] Arr= {{-3., -2.}, {-2., -1.}, {-1., 0.}, {0., 1.}, {0.5, 45.}, {1., 2.}, {2., 3.}, {3., 4.}};
+        eq.addPoint(new FunctionPoint(0.5, 45));
+        for(int i = 0; i < eq.getPointsCount(); i++) {
+            Assertions.assertEquals(eq.getPointX(i),Arr[i][0]);
+            Assertions.assertEquals(eq.getPointY(i),Arr[i][1]);
+        }
+    }
+    @Test
+    public void tabulatedFunctionDeletePointTest(){
+        Double[][] Arr= {{-3., -2.}, {-2., -1.}, {-1., 0.}, {0., 1.}, {1., 2.}, {3., 4.}};
+        eq.deletePoint(5);
+        for(int i = 0; i < eq.getPointsCount(); i++) {
+            Assertions.assertEquals(eq.getPointX(i),Arr[i][0]);
+            Assertions.assertEquals(eq.getPointY(i),Arr[i][1]);
+        }
+    }
+    @Test
+    public void tabulatedFunctionSetPointXTest(){
+        Double[][] Arr= {{-5., -2.}, {-2., -1.}, {-1., 0.}, {0., 1.}, {1., 2.}, {2., 3.}, {3., 4.}};
+        eq.setPointX(0, -5);
+        for(int i = 0; i < eq.getPointsCount(); i++) {
+            Assertions.assertEquals(eq.getPointX(i),Arr[i][0]);
+            Assertions.assertEquals(eq.getPointY(i),Arr[i][1]);
+        }
+    }
+    @Test
+    public void tabulatedFunctionSetPointYTest(){
+        Double[][] Arr= {{-3., -2.}, {-2., -1.}, {-1., 0.}, {0., 1.}, {1., 1000.}, {2., 3.}, {3., 4.}};
+        eq.setPointY(4, 1000);
+        for(int i = 0; i < eq.getPointsCount(); i++) {
+            Assertions.assertEquals(eq.getPointX(i),Arr[i][0]);
+            Assertions.assertEquals(eq.getPointY(i),Arr[i][1]);
+        }
+    }
+    @Test
+    public void tabulatedFunctionSetPointTest(){
+        Double[][] Arr= {{-3., -2.}, {-2., -1.}, {-0.7, -42.875}, {0., 1.}, {1., 2.}, {2., 3.}, {3., 4.}};
+        eq.setPoint(2, new FunctionPoint(-0.7, -42.875));
+        for(int i = 0; i < eq.getPointsCount(); i++) {
+            Assertions.assertEquals(eq.getPointX(i),Arr[i][0]);
+            Assertions.assertEquals(eq.getPointY(i),Arr[i][1]);
+        }
+    }
+}
