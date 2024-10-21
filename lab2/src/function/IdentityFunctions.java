f75159a35ccfc2afce6ac8cb20a783fd6c95d70f
Daniil Krovyakov <mr.krovyakov.daniil@gmail.com>
2024-10-21 20:47:34 +0400

    Task 1

--- lab2/src/function/IdentityFunctions.java
+++ lab2/src/function/IdentityFunctions.java
@@ -4,4 +4,21 @@ public class IdentityFunctions implements MathFunction{
     public double apply(double x) {
         return x;
     }
+
+    public String toString(){
+        return "";
+    }
+
+    public boolean equals(Object o){
+        return o instanceof IdentityFunctions;
+    }
+
+//    @Override
+//    public int hashCode(){
+//        return;
+//    }
+
+    public Object clone(){
+        return new IdentityFunctions();
+    }
 }
