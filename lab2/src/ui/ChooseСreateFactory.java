a67b054758209caadbfc1a10d00be21ff32dc98e
Daniil Krovyakov <mr.krovyakov.daniil@gmail.com>
2024-12-16 12:23:02 +0400

    create a design for createFactory

--- lab2/src/ui/ChooseСreateFactory.java
+++ lab2/src/ui/ChooseСreateFactory.java
@@ -4,14 +4,40 @@ import javax.swing.*;
 import function.factory.ArrayTabulatedFunctionFactory;
 import function.factory.LinkedListTabulatedFunctionFactory;
 import operations.TabulatedFunctionOperationService;
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class ChooseСreateFactory extends JDialog {
+    private final int WIDTH_DIALOG = 400;
+    private final int HEIGHT_DIALOG = 250;
+
     public ChooseСreateFactory(JFrame owner, TabulatedFunctionOperationService factoryService) {
         super(owner, "Настройки", true); // Модальное окно
-        setSize(400, 200);
+        setSize(WIDTH_DIALOG, HEIGHT_DIALOG);
         setLocationRelativeTo(null);
+
+        JPanel gradientPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+
+                // Устанавливаем качественные отображение
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                        RenderingHints.VALUE_ANTIALIAS_ON);
+
+                // Создаем градиентный фон
+                GradientPaint gp = new GradientPaint(200, 0, new Color(237, 199, 183), 0, getHeight(), new Color(172, 59, 97)); // Нижняя часть фона (более светлый серый)
+
+                g2d.setPaint(gp);
+                g2d.fillRect(0, 0, getWidth(), getHeight());
+            }
+        };
 
         JRadioButton arrayFactoryButton = new JRadioButton("Фабрика на основе массива", factoryService.factoryGetter() instanceof ArrayTabulatedFunctionFactory);
         JRadioButton listFactoryButton = new JRadioButton("Фабрика на основе связного списка", factoryService.factoryGetter() instanceof LinkedListTabulatedFunctionFactory);
@@ -30,11 +56,11 @@ public class ChooseСreateFactory extends JDialog {
             dispose();
         });
 
-        JPanel panel = new JPanel();
-        panel.add(arrayFactoryButton);
-        panel.add(listFactoryButton);
-        panel.add(saveButton);
-        add(panel);
+        gradientPanel.add(arrayFactoryButton);
+        gradientPanel.add(listFactoryButton);
+        gradientPanel.add(saveButton);
+
+        add(gradientPanel);
 
         addWindowListener(new WindowAdapter() {
             @Override
